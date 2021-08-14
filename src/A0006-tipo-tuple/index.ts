a68e25668b357af1eb24da368f3a61fd2bd3d70b
Bianca <bruninhakjurugat11@gmail.com>
2021-08-14 01:46:00 -0300

    A0006-tipo-tuple

---
+++ src/A0006-tipo-tuple/index.ts
@@ -0,0 +1,12 @@
+const dadosCliente: [number, string] = [1, 'Luiz'];
+const dadosCliente2: [number, string, ...string[]] = [
+  1,
+  'Luiz',
+  'Otávio',
+  'Miranda',
+];
+const dadosCliente3: readonly [number, string] = [1, 'Luiz'];
+const array: ReadonlyArray<string> = ['Luiz', 'Otávio'];
+
+dadosCliente.pop(); //Funciona!
+// dadosCliente3.pop();
